fad3b8b22c9232a2deb78e711b43925abad15253
paloma serna <palomaserna55@gmail.com>
2025-06-09 22:30:56 -0300

    rama

--- prySerna_IEFI/frmAuditoria.Designer.cs
+++ prySerna_IEFI/frmAuditoria.Designer.cs
@@ -30,19 +30,91 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAuditoria));
             this.dgvAuditoria = new System.Windows.Forms.DataGridView();
+            this.grpTarea = new System.Windows.Forms.GroupBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.nmCodigo = new System.Windows.Forms.NumericUpDown();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCodigoUser = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAuditoria)).BeginInit();
+            this.grpTarea.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmCodigo)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvAuditoria
             // 
             this.dgvAuditoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAuditoria.Location = new System.Drawing.Point(106, 114);
+            this.dgvAuditoria.Location = new System.Drawing.Point(121, 168);
             this.dgvAuditoria.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvAuditoria.Name = "dgvAuditoria";
             this.dgvAuditoria.ReadOnly = true;
             this.dgvAuditoria.RowHeadersWidth = 62;
             this.dgvAuditoria.Size = new System.Drawing.Size(958, 488);
             this.dgvAuditoria.TabIndex = 0;
+            // 
+            // grpTarea
+            // 
+            this.grpTarea.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("grpTarea.BackgroundImage")));
+            this.grpTarea.Controls.Add(this.txtUsuario);
+            this.grpTarea.Controls.Add(this.nmCodigo);
+            this.grpTarea.Controls.Add(this.lblNombre);
+            this.grpTarea.Controls.Add(this.lblCodigoUser);
+            this.grpTarea.Controls.Add(this.btnBuscar);
+            this.grpTarea.Font = new System.Drawing.Font("Impact", 9F);
+            this.grpTarea.ForeColor = System.Drawing.Color.White;
+            this.grpTarea.Location = new System.Drawing.Point(121, 30);
+            this.grpTarea.Name = "grpTarea";
+            this.grpTarea.Size = new System.Drawing.Size(903, 120);
+            this.grpTarea.TabIndex = 10;
+            this.grpTarea.TabStop = false;
+            this.grpTarea.Text = "Búsqueda de usuarios";
+          
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(518, 40);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(131, 29);
+            this.txtUsuario.TabIndex = 14;
+            // 
+            // nmCodigo
+            // 
+            this.nmCodigo.Location = new System.Drawing.Point(172, 40);
+            this.nmCodigo.Name = "nmCodigo";
+            this.nmCodigo.Size = new System.Drawing.Size(120, 29);
+            this.nmCodigo.TabIndex = 13;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.Color.Transparent;
+            this.lblNombre.Location = new System.Drawing.Point(348, 47);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(146, 22);
+            this.lblNombre.TabIndex = 12;
+            this.lblNombre.Text = "Nombre de Usuario";
+            // 
+            // lblCodigoUser
+            // 
+            this.lblCodigoUser.AutoSize = true;
+            this.lblCodigoUser.BackColor = System.Drawing.Color.Transparent;
+            this.lblCodigoUser.Location = new System.Drawing.Point(19, 46);
+            this.lblCodigoUser.Name = "lblCodigoUser";
+            this.lblCodigoUser.Size = new System.Drawing.Size(141, 22);
+            this.lblCodigoUser.TabIndex = 11;
+            this.lblCodigoUser.Text = "Código de Usuario";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(133)))), ((int)(((byte)(141)))));
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBuscar.Location = new System.Drawing.Point(696, 36);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(114, 36);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // frmAuditoria
             // 
@@ -51,14 +123,18 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.grpTarea);
             this.Controls.Add(this.dgvAuditoria);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmAuditoria";
             this.Text = "Auditoria";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmAuditoria_FormClosing);
+            
             this.Load += new System.EventHandler(this.frmAuditoria_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvAuditoria)).EndInit();
+            this.grpTarea.ResumeLayout(false);
+            this.grpTarea.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmCodigo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -66,5 +142,11 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvAuditoria;
+        private System.Windows.Forms.GroupBox grpTarea;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.NumericUpDown nmCodigo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCodigoUser;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
--- prySerna_IEFI/frmAuditoria.cs
+++ prySerna_IEFI/frmAuditoria.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -23,9 +24,63 @@ namespace prySerna_IEFI
             BD.CargarUsuarios(dgvAuditoria);
         }
 
-        private void frmAuditoria_FormClosing(object sender, FormClosingEventArgs e)
+
+
+
+
+        private void btnBuscar_Click(object sender, EventArgs e)
         {
-            
-        }
+            try
+            {
+                clsConexión BD = new clsConexión();
+
+                string query = @"
+              SELECT 
+              U.IdUsuario,
+              U.Usuario,
+              U.Contraseña,
+              U.Rol,
+              U.FechaCreacion,
+              U.UltimaConexion,
+              U.TiempoUltimaConexion,
+              U.TiempoTotal,
+              U.Estado
+              FROM Usuarios U
+              WHERE 1 = 1";
+
+                using (SqlConnection conexion = new SqlConnection(BD.cadenaConexion))
+                using (SqlCommand comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+
+                    if (nmCodigo.Value > 0)
+                    {
+                        query += " AND U.IdUsuario = @IdUsuario";
+                        comando.Parameters.AddWithValue("@IdUsuario", Convert.ToInt32(nmCodigo.Value));
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(txtUsuario.Text))
+                    {
+                        query += " AND U.Usuario LIKE @NombreUsuario";
+                        comando.Parameters.AddWithValue("@NombreUsuario", "%" + txtUsuario.Text + "%");
+                    }
+
+                    comando.CommandText = query;
+                    conexion.Open();
+                    using (SqlDataReader reader = comando.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        dgvAuditoria.DataSource = dt;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al buscar el usuario: " + ex.Message);
+            }
+
+            }
     }
 }
